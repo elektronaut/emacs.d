6b7be30bcf7d91b311da007e1afeb6dfb38959c3
Inge Jørgensen <inge@anyone.no>
2017-06-19 16:13:56 +0200

    Don't override windmove keys in org buffers

--- modules/module-org.el
+++ modules/module-org.el
@@ -19,6 +19,31 @@
       org-hide-leading-stars nil
       org-log-done t)
 
+(with-eval-after-load 'org
+  ;; Prevent Org from overriding the bindings for windmove.
+  (define-key org-mode-map (kbd "S-<left>") nil)
+  (define-key org-mode-map (kbd "S-<right>") nil)
+  (define-key org-mode-map (kbd "S-<up>") nil)
+  (define-key org-mode-map (kbd "S-<down>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<up>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<down>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<left>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<right>") nil)
+
+  ;; Add replacements for the some of keybindings we just removed. It
+  ;; looks like Org already binds C-up and C-down separately from M-{
+  ;; and M-}, so we can't use those. Users will just have to make do
+  ;; with C-c <up> and C-c <down> for now.
+  ;;
+  ;; Now for Org Agenda on the other hand, we could use C-up and
+  ;; C-down because M-{ and M-} are bound to the same commands. But I
+  ;; think it's best to take the same approach as before, for
+  ;; consistency.
+  (define-key org-mode-map (kbd "C-<left>") #'org-shiftleft)
+  (define-key org-mode-map (kbd "C-<right>") #'org-shiftright)
+  (define-key org-agenda-mode-map (kbd "C-<left>") #'org-agenda-do-date-earlier)
+  (define-key org-agenda-mode-map (kbd "C-<right>") #'org-agenda-do-date-later))
+
 (use-package org-journal
   :init
   (setq org-journal-dir "~/Dropbox/org/journal"))
