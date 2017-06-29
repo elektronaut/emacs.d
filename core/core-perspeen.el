c7639e509d6fd0f741ef002adadb026652aad165
Inge Jørgensen <inge@anyone.no>
2017-06-29 16:15:56 +0200

    Enable perspeen

---
+++ core/core-perspeen.el
@@ -0,0 +1,11 @@
+;;; core-perspeen.el --- Perspeen
+;;; Commentary:
+;;; Code:
+
+(use-package perspeen
+  :ensure t
+  :config
+  (perspeen-mode))
+
+(provide 'core-perspeen)
+;;; core-perspeen.el ends here
