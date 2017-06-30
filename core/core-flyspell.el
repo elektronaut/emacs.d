20085e4f6c35ccb95ad00e35d68b120ce5b6a55b
Inge Jørgensen <inge@anyone.no>
2017-06-30 11:24:21 +0200

    Don't enable flyspell automatically

--- core/core-flyspell.el
+++ core/core-flyspell.el
@@ -11,8 +11,8 @@
   :config
   (setq ispell-program-name "aspell" ; use aspell instead of ispell
         ispell-extra-args '("--sug-mode=ultra"))
-  (add-hook 'text-mode-hook 'flyspell-mode)
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  ; (add-hook 'text-mode-hook 'flyspell-mode)
+  ; (add-hook 'prog-mode-hook 'flyspell-prog-mode)
   (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
   (define-key flyspell-mouse-map [mouse-3] #'undefined))
 
