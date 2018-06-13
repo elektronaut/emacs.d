88c5b4ff7640c19f16593e41be835d9516f98fa4
Inge Jørgensen <inge@manualdesign.no>
2018-06-13 02:42:59 +0200

    random-hex-token function

--- core/core-defuns.el
+++ core/core-defuns.el
@@ -2,6 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun random-hex-token (length)
+  "Generate a random hexadecimal token of LENGTH using OpenSSL."
+  (interactive "sLength (Default 16): ")
+  (let* ((len (if (string= "" length) "16" length))
+         (token (replace-regexp-in-string
+                 "\n\\'" ""
+                 (shell-command-to-string (concat "openssl rand -hex " len)))))
+    (if (and transient-mark-mode mark-active)
+        (delete-region (region-beginning) (region-end)))
+    (insert token)))
+
 (defun split-n (n)
   "Split frame in N windows."
   (let ((split-count (- n (count-windows))))
