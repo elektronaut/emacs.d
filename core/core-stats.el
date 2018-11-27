2b098a5ed6fd66db346c8f80c31c7684f38355ac
Inge Jørgensen <inge@manualdesign.no>
2018-11-27 03:55:28 +0100

    Track stats with keyfreq

---
+++ core/core-stats.el
@@ -0,0 +1,16 @@
+;;; core-stats -- Stats
+;;; Author: Inge Jørgensen <inge@elektronaut.no>
+
+;;; Commentary:
+
+;;; Licence: MIT
+
+;;; Code:
+
+(use-package keyfreq
+  :config
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1))
+
+(provide 'core-stats)
+;;; core-stats ends here
