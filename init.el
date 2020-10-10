96db8d7c25f5726eeca9d079862dccafabb11d46
Inge Jørgensen <inge@anyone.no>
2020-10-10 03:45:45 +0200

    Init and GC tweaks

--- init.el
+++ init.el
@@ -13,11 +13,8 @@
 ;; Startup
 ;;-----------------------------------------------------------------------------
 
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
-
-(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold 16777216
-                                               gc-cons-percentage 0.1)))
+; Defer garbage collection for now. gcmh-mode takes care of this later.
+(setq gc-cons-threshold most-positive-fixnum)
 
 (add-hook 'emacs-startup-hook
           (lambda ()
