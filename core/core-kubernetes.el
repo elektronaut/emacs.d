97d12b823cb8985f2c0d6ee2f25380ad140d1a02
Inge Jørgensen <inge@elektronaut.no>
2024-02-29 17:00:04 +0100

    Add kubernetes package

---
+++ core/core-kubernetes.el
@@ -0,0 +1,10 @@
+;;; core-kubernetes.el --- Settings for kubernetes
+;;; Commentary:
+;;; Code:
+
+(use-package kubernetes
+  :ensure t
+  :defer t)
+
+(provide 'core-kubernetes)
+;;; core-kubernetes.el ends here
