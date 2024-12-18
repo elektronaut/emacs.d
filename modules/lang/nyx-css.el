076a2d2e47317222616861a0b282895877491b69
Inge Jørgensen <inge@elektronaut.no>
2024-12-18 11:51:50 +0100

    Add css-toggle-px-rem for toggling between sizes in CSS files

--- modules/lang/nyx-css.el
+++ modules/lang/nyx-css.el
@@ -2,8 +2,35 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar-local css-base-rem-size 16
+  "Base font size in pixels for px to rem conversion.")
+
+(defun css-toggle-px-rem ()
+  "Toggle between px and rem for number at point or in region.
+Uses `css-base-rem-size' (defaults to 16) as the base for conversion."
+  (interactive)
+  (let* ((bounds (if (use-region-p)
+                     (cons (region-beginning) (region-end))
+                   (bounds-of-thing-at-point 'symbol)))
+         (number-str (when bounds
+                       (buffer-substring-no-properties (car bounds) (cdr bounds))))
+         (base (buffer-local-value 'css-base-rem-size (current-buffer))))
+    (when (and bounds number-str)
+      (let* ((is-px (string-match-p "px$" number-str))
+             (is-rem (string-match-p "rem$" number-str))
+             (num (string-to-number number-str))
+             (new-str (cond
+                       (is-px (format "%grem" (/ num (float base))))
+                       (is-rem (format "%dpx" (round (* num base))))
+                       (t number-str))))
+        (save-excursion
+          (delete-region (car bounds) (cdr bounds))
+          (goto-char (car bounds))
+          (insert new-str))))))
+
 (use-package css-mode
   :ensure nil
+  :bind (("C-c C-t" . css-toggle-px-rem))
   :mode (("\\.css\\'"     . scss-mode)
          ("\\.postcss\\'" . scss-mode)
          ("\\.pcss\\'"    . scss-mode)
